8be72c61fdeaff531a3712fc7a4f64274d0ce5f6
jmerten <jmerte@gmail.com>
2021-09-13 09:01:31 -0400

    Add files via upload

---
+++ final_integration.sql
@@ -0,0 +1,8 @@
+create database final_integration;
+use final_integration;
+create table distance (
+	source varchar(40),
+    destination varchar(40),
+    distance double);
+drop table distance;
+
